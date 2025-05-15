35bb3eb676b2de3e236a3e35bc310e2380cea6bc
sonu786786 <psonu786786@gmail.com>
2025-05-15 08:38:44 +0530

    conversion of dataTypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valInNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber)
+// console.log(valInNumber + 3) 
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1, false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn) // 0 => false, 1 => true
+
+// 1 => true, 0 => false
+// "abc" => true, "" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) // "33"
